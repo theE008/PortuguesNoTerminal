2fe146a7f9eb6d5a3aba76ce8019569e18f2e419
theE008 <pie2eulerman@gmail.com>
2025-02-22 09:03:38 -0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PortuguesNoTerminal
 Um arquivo que, se adicionado no PATH do sistema windows, te permite executar comandos totalmente em português pelo terminal
+
+É necessário que este arquivo esteja dentro de seuUsuário/cmd
--- fazer/commit.bat
+++ fazer/commit.bat
@@ -1,4 +1,12 @@
 @echo off
 git add .
 git commit -m %1
+
+:: Check if the current branch is master before renaming
+for /f "tokens=*" %%a in ('git symbolic-ref --short HEAD') do set branch=%%a
+if "%branch%"=="master" (
+    git branch -m master main
+)
+
 git push origin main
+git push --set-upstream origin main
